fa366453b49d54c156a5a9c81a3bf2f3f1e1b0e6
dengziyang <dengziyang@meicai.cn>
2018-05-28 20:45:47 +0800

    d

---
+++ src/main/java/com/dzy/test/SparkWordCount.java
@@ -0,0 +1,26 @@
+package com.dzy.test;
+
+
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.rdd.RDD;
+import org.apache.spark.sql.SparkSession;
+
+
+public class SparkWordCount {
+    public static void main(String[] args){
+        SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
+        SparkContext sc = spark.sparkContext();
+        RDD<String> rdd = sc.textFile("hdfs://ns1/user/ruban/test/wc/input/input.txt", 20);
+        JavaRDD<String> javaRDD = rdd.toJavaRDD();
+        javaRDD.foreach(new VoidFunction<String>() {
+            @Override
+            public void call(String s) throws Exception {
+                System.out.println(s);
+            }
+        });
+
+        spark.stop();
+    }
+}
